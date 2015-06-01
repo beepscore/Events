2f72a1bc5bdff71e6a63c34444e1cca1a9615717
beepscore <support@beepscore.com>
2015-05-31 23:53:17 -0700

    In MainActivity add events, addEvent, getEvents, showEvents. In onCreate use them.
    TODO: replace deprecated method startManagingCursor.

--- app/src/main/java/com/beepscore/android/events/MainActivity.java
+++ app/src/main/java/com/beepscore/android/events/MainActivity.java
@@ -1,17 +1,40 @@
 package com.beepscore.android.events;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import static android.provider.BaseColumns._ID;
+import static com.beepscore.android.events.Constants.TABLE_NAME;
+import static com.beepscore.android.events.Constants.TIME;
+import static com.beepscore.android.events.Constants.TITLE;
 
 
 public class MainActivity extends Activity {
+
+    private EventsData events;
+    private static String[] FROM = { _ID, TIME, TITLE, };
+    private static String ORDER_BY = TIME + " DESC";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        events = new EventsData(this);
+        try {
+            addEvent("Hello Android!");
+            Cursor cursor = getEvents();
+            showEvents(cursor);
+        } finally {
+            events.close();
+        }
     }
 
     @Override
@@ -34,5 +57,45 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addEvent(String title) {
+        // Insert a new record into the Events data source.
+        // You would do something similar for delete and update.
+        SQLiteDatabase db = events.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(TIME, System.currentTimeMillis());
+        values.put(TITLE, title);
+        db.insertOrThrow(TABLE_NAME, null, values);
+    }
+
+    private Cursor getEvents() {
+        // Perform a managed query. The Activity will handle closing
+        // and re-querying the cursor when needed.
+        SQLiteDatabase db = events.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAME, FROM,
+                null, null, null, null, ORDER_BY);
+        // TODO: replace deprecated method startManagingCursor
+        startManagingCursor(cursor);
+        return cursor;
+    }
+
+    private void showEvents(Cursor cursor) {
+        // Stuff them all into a big string
+        StringBuilder builder = new StringBuilder("Saved events:\n");
+
+        while (cursor.moveToNext()) {
+            // Could use getColumnIndexOrThrow() to get indexes
+            long id = cursor.getLong(0);
+            long time = cursor.getLong(1);
+            String title = cursor.getString(2);
+
+            builder.append(id).append(": ");
+            builder.append(time).append(": ");
+            builder.append(title).append("\n");
+        }
+        // Display on the screen
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText(builder);
     }
 }
